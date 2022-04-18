92289aa2046bf4b70141dbc34130e9657b7c0ce6
Lint Action <lint-action@samuelmeuli.com>
2022-04-18 05:10:44 +0000

    Fix code style issues with Prettier

--- src/contracts/DAOTreasury.sol
+++ src/contracts/DAOTreasury.sol
@@ -5,4 +5,6 @@ pragma solidity ^0.8.4;
 
 // TODO: think about contract deploymnet pirority and change NFT contract constructor
 // TODO: write this function pausable and in future proof maner
-contract DAOTreasury {}
+contract DAOTreasury {
+
+}
