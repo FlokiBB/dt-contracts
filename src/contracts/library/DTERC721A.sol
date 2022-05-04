4e6755075918698d613937af1275b6caeffdfaea
FlokiBB <FlokiBB@protonmail.com>
2022-05-04 10:48:25 +0430

    Refactor(DTERC721A.sol): fix dictation

--- src/contracts/library/DTERC721A.sol
+++ src/contracts/library/DTERC721A.sol
@@ -282,7 +282,7 @@ contract DTERC721A is ERC165, IERC721, IERC721Metadata, IERC721Enumerable {
     /**
      * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
      * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
-     * by default, can be overriden in child contracts.
+     * by default, can be overridden in child contracts.
      */
     function _baseURI() internal view virtual returns (string memory) {
         return '';
