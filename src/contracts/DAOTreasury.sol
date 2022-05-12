ee0d47b1540a2eb1e1a904ae4b1df118c0fb1c6b
Lint Action <lint-action@samuelmeuli.com>
2022-05-12 12:53:47 +0000

    Fix code style issues with Prettier

--- src/contracts/DAOTreasury.sol
+++ src/contracts/DAOTreasury.sol
@@ -9,7 +9,6 @@ import './interfaces/ICollectiGame.sol';
 import './interfaces/IDAOTreasury.sol';
 import './interfaces/IGameTreasury.sol';
 
-
 //Note: unlockDate, add team address, add DaoProposalFundingTransaction
 contract DAOTreasury is UUPSUpgradeable, DTAuth(1), IDAOTreasury {
     uint256 public immutable START_TIME = block.timestamp;
@@ -24,7 +23,6 @@ contract DAOTreasury is UUPSUpgradeable, DTAuth(1), IDAOTreasury {
     uint256 public daoProposalFundingStartTime;
     bool private isSetup = false;
 
-
     struct Release {
         uint256 amountOrPercent;
         uint256 releaseDate;
@@ -33,7 +31,6 @@ contract DAOTreasury is UUPSUpgradeable, DTAuth(1), IDAOTreasury {
     }
     mapping(uint8 => Release) public ethReleasesPlan;
     mapping(uint8 => string) public ethReleasesPlanDescription;
-
 
     event ChangeAnnouncement(address daoMultisig, address newImplementation);
 
@@ -97,10 +94,15 @@ contract DAOTreasury is UUPSUpgradeable, DTAuth(1), IDAOTreasury {
         require(!isSetup, 'Already Setup');
         uint8 releaseId = 0;
 
-        uint256 duringMintingReleaseAmount = collectigameSupply * guaranteedFlorPrice * 20 / 100;
-        Release memory duringMintingRelease = Release(duringMintingReleaseAmount, START_TIME, roles[DAO_ROLE_ID].addr, false);
+        uint256 duringMintingReleaseAmount = (collectigameSupply * guaranteedFlorPrice * 20) / 100;
+        Release memory duringMintingRelease = Release(
+            duringMintingReleaseAmount,
+            START_TIME,
+            roles[DAO_ROLE_ID].addr,
+            false
+        );
         ethReleasesPlan[releaseId] = duringMintingRelease;
-        ethReleasesPlanDescription[releaseId] = "Market making and bootstrapping";
+        ethReleasesPlanDescription[releaseId] = 'Market making and bootstrapping';
         releaseId++;
 
         uint256 month = 3 * 30 * 24 * 60 * 60;
@@ -108,18 +110,18 @@ contract DAOTreasury is UUPSUpgradeable, DTAuth(1), IDAOTreasury {
             uint256 releasePercent = 10;
             uint256 releaseDate = START_TIME + (3 * month) + ((releaseId - 1) * month);
 
-            Release memory release = Release(releasePercent, releaseDate, teamMultisig , false);
+            Release memory release = Release(releasePercent, releaseDate, teamMultisig, false);
             ethReleasesPlan[releaseId] = release;
-            ethReleasesPlanDescription[releaseId] = "The team";
+            ethReleasesPlanDescription[releaseId] = 'The team';
         }
 
         for (releaseId; releaseId < 7; releaseId++) {
             uint256 releasePercent = 10;
             uint256 releaseDate = START_TIME + (3 * month) + ((releaseId - 1) * month);
-            
-            Release memory release = Release(releasePercent, releaseDate, roles[DAO_ROLE_ID].addr , false);
+
+            Release memory release = Release(releasePercent, releaseDate, roles[DAO_ROLE_ID].addr, false);
             ethReleasesPlan[releaseId] = release;
-            ethReleasesPlanDescription[releaseId] = "DDD platform expansion";
+            ethReleasesPlanDescription[releaseId] = 'DDD platform expansion';
         }
 
         isSetup = true;
@@ -159,12 +161,11 @@ contract DAOTreasury is UUPSUpgradeable, DTAuth(1), IDAOTreasury {
             _transferEth(release.receiver, release.amountOrPercent);
         } else {
             release.isReleased = true;
-            
+
             uint256 treasuryBalance = getTreasuryBalance();
-            uint256 releaseAmount = treasuryBalance * release.amountOrPercent / 100;
+            uint256 releaseAmount = (treasuryBalance * release.amountOrPercent) / 100;
             _transferEth(release.receiver, releaseAmount);
         }
-
     }
 
     function _transferEth(address to_, uint256 amount) internal virtual {
@@ -172,7 +173,6 @@ contract DAOTreasury is UUPSUpgradeable, DTAuth(1), IDAOTreasury {
         (bool sent, ) = to.call{value: amount}('');
         require(sent, 'Transfer Failed');
     }
-
 
     function _authorizeUpgrade(address newImplementation) internal virtual override {
         address daoMultisig = roles[DAO_ROLE_ID].addr;
